829663358cdd6726249625cd13a17476e26abe0c
acse-moj20 <moj20@ic.ac.uk>
2021-03-28 19:50:51 +0100

    updatin files with req

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var rate = document.getElementById("interest").value;
+    var years = document.getElementById("years").value;
+    var interest = p * years * rate / 100;
+    var date = new Date();
+    var fut_year = (date + years).getFullYear() ;
+
+    if (p <= 0) {
+        window.alert(" Please Enter a Positive Number for Amount.");
+        document.getElementById("principal").focus();
+    }
+    else {
+        document.getElementById("result").innerHTML =
+         "If you deposit " + "<mark>" + p + "</mark>"+",<br/>" +
+        " at an interest rate of "+" <mark>" + rate + "%"+"</mark>"+"<br />" +
+         "You will receive an amount of "+"<mark>" + interest + "</mark>"+",<br />" +
+         "in the year "+ "<mark>" + fut_year + "</mark>";
+    }
+}
+
+function updateText(value) {
+    document.getElementById("rate").innerHTML = value;
+}
+
+function show_interest() {
+
 }
         
